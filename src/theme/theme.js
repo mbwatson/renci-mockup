ff2db26e9bbabd1ffbb9dfdc98d98f433fe35f9c
Matt Watson <mattwat@gmail.com>
2020-05-11 16:02:59 -0400

    add starter theme

---
+++ src/theme/theme.js
@@ -0,0 +1,35 @@
+export const theme = {
+    color: {
+        carolinaBlue: '#4b9cd3',
+        renciBlue: '#00758d',
+        primary: {
+            light: '#00a5cd',
+            main: '#00758d',
+            dark: '#00455d',
+        },
+        extended: {
+            moss: '#5e9b8b',
+            contessa: '#c16a5d',
+            sherbet: '#ffa872',
+        },
+        white: '#eff',
+        black: '#222',
+        darkgrey: '#334',
+        grey: '#999',
+        lightgrey: '#cdd',
+        success: '#5e9b8b',
+        info: '#49a0d9',
+        warning: '#ffa872',
+        danger: '#c16a5d',
+    },
+    border: {
+        radius: '3px',
+        width: '1px',
+    },
+    padding: {
+        small: '1rem',
+        normal: '2rem',
+        large: '3rem',
+        extraLarge: '4rem',
+    },
+}
