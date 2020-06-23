26500956aa0cfa3e7069f52164aab00b51a36120
Matt Watson <mattwat@gmail.com>
2020-06-23 15:49:56 -0400

    extract spotlight hook into separate file

--- src/hooks/use-news-spotlight.js
+++ src/hooks/use-news-spotlight.js
@@ -1,19 +1,19 @@
 import { graphql, useStaticQuery } from 'gatsby'
 
 const newsQuery = graphql`{
-    news: allMarkdownRemark(sort: {fields: frontmatter___publish_date, order: DESC}) {
-        edges {
-            node {
-                id
-                frontmatter {
-                    title
-                    spotlight
+   spotlight: allMarkdownRemark(sort: {fields: frontmatter___publish_date, order: DESC}, filter: {frontmatter: {spotlight: {eq: true}}}) {
+      edges {
+         node {
+            id
+            frontmatter {
+                title
+                spotlight
                     featuredImage {
                         childImageSharp {
                             fullSize: fluid {
                                 ...GatsbyImageSharpFluid
                             }
-                            previewSize: fixed(width: 300, height: 300) {
+                            previewSize: fixed(width: 400, height: 400) {
                                 ...GatsbyImageSharpFixed
                             }
                         }
@@ -24,14 +24,13 @@ const newsQuery = graphql`{
                         name
                     }
                 }
-                html
                 excerpt(pruneLength: 500)
             }
         }
     }
 }`
 
-export const useNews = () => {
-    const { news } = useStaticQuery(newsQuery)
-    return news.edges.map(({ node }) => node)
+export const useNewsSpotlight = () => {
+    const { spotlight } = useStaticQuery(newsQuery)
+    return spotlight.edges.map(({ node }) => node)
 }
