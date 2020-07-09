df1368e7c4233112aed898c936ff6aa4e12d56b7
Matt Watson <mattwat@gmail.com>
2020-07-09 17:28:36 -0400

    work out client-side warings and flesh out hero component

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import Img from 'gatsby-image'
 import { useScrollPosition } from '../../hooks'
@@ -16,37 +17,57 @@ const Content = styled.div(({ theme }) => `
     width: 100%;
     max-width: 1200px;
     margin: 0 auto;
+    background-color: transparent;
     color: ${ theme.color.whitish };
     h1 {
         color: ${ theme.color.lightgrey };
     }
 `)
 
-const Wrapper = styled.div(({ theme }) => `
-    position: relative;
-    background: ${ theme.color.black };
-    min-height: 300px;
-    overflow: hidden;
-    &::after {
-        content: "";
-        position: absolute;
-        left: 0;
-        right: 0;
-        top: 0;
-        bottom: 0;
-        background-color: ${ theme.color.renciBlue }33;
-    }
+const Overlay = styled.div(({ theme, color }) => `
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: ${ color };
+    opacity: 0.1;
 `)
 
-export const Hero = ({ backgroundImage, children }) => {
+const Wrapper = styled.div(({ theme }) => `
+    position: relative;
+    min-height: 300px;
+    overflow: hidden;
+`)
+
+export const Hero = ({ backgroundImage, overlayColor, children }) => {
     const { scrollPosition } = useScrollPosition()
 
     return (
         <Wrapper>
-            { backgroundImage && <Img fluid={ backgroundImage } style={{ height: '300px' }} imgStyle={{ transform: `translateY(${ scrollPosition / 2 }px)` }} /> }
+            {
+                backgroundImage && (
+                    <Img
+                        fluid={ backgroundImage }
+                        style={{ height: '300px' }}
+                        imgStyle={{ transform: `translateY(${ scrollPosition / 2 }px)` }}
+                    />
+                )
+            }
+            <Overlay color={ overlayColor } />
             <Content>
                 { children }
             </Content>
         </Wrapper>
     )
+}
+
+Hero.propTypes = {
+    backgroundImage: PropTypes.object,
+    overlayColor: PropTypes.string.isRequired,
+    children: PropTypes.node,
+}
+
+Hero.defaultProps = {
+    overlayColor: '#fff',
 }
--- src/pages/careers.js
+++ src/pages/careers.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { SEO } from '../components/seo'
 import { Container, Section } from '../components/layout'
+import { Hero } from '../components/hero'
 import { Title, Paragraph } from '../components/typography'
 import { HorizontalRule } from '../components/horizontal-rule'
 import { useCareers } from '../hooks'
@@ -10,35 +11,41 @@ const PublicationsPage = () => {
     const careers = useCareers()
     
     return (
-        <Container>
+        <Fragment>
             <SEO title="Careers" />
-            
-            <Title>Careers</Title>
 
-            <Paragraph>
-                RENCI's inclusive, creative ethod transcende disciplinary boundaries to catalyze data-driven discovery and innovation for the public good.
-                A career at RENCI offers a stimulating, collaborative environment where you can unleash your intellectual potential to advance technology and accelereate research.
-                We look forward to meeting you!
-            </Paragraph>
+            <Hero>
+                <Title>Careers</Title>
 
-            <HorizontalRule />
-            
-            <Section title="Open Positions">
-                {
-                    careers.map(posting => (
-                        <JobPosting
-                            key={ posting.id }
-                            title={ posting.frontmatter.title }
-                            publishDate={ posting.frontmatter.publish_date }
-                            positionNumber={ posting.frontmatter.unc_position_number }
-                            description={ posting.html }
-                            url={ `http://unc.peopleadmin.com/postings/${ posting.frontmatter.unc_position_number }` }
-                        />
-                    ))
-                }
-            </Section>
+                <Paragraph>
+                    RENCI's inclusive, creative ethod transcende disciplinary boundaries to catalyze data-driven discovery and innovation for the public good.
+                    A career at RENCI offers a stimulating, collaborative environment where you can unleash your intellectual potential to advance technology and accelereate research.
+                    We look forward to meeting you!
+                </Paragraph>
+            </Hero>
 
-        </Container>
+            <Container>
+                
+
+                <HorizontalRule />
+                
+                <Section title="Open Positions">
+                    {
+                        careers.map(posting => (
+                            <JobPosting
+                                key={ posting.id }
+                                title={ posting.frontmatter.title }
+                                publishDate={ posting.frontmatter.publish_date }
+                                positionNumber={ posting.frontmatter.unc_position_number }
+                                description={ posting.html }
+                                url={ `http://unc.peopleadmin.com/postings/${ posting.frontmatter.unc_position_number }` }
+                            />
+                        ))
+                    }
+                </Section>
+
+            </Container>
+        </Fragment>
     )
 }
 
