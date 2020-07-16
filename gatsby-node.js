4ac0afbd5db2e08c4c98c68ba2ac5c939d3f62c9
Matt Watson <mattwat@gmail.com>
2020-07-16 12:57:42 -0400

    add commented path field additions

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,8 +14,11 @@ exports.onCreateNode = ({ node, actions }) => {
             })
         }
     }
-    // Transform the new node here and create a new node or
-    // create a new node field.
+    // if (node.internal.type === 'PeopleYaml') { createNodeField({ node, name: 'path', value: `/people/${ node.id }` }) }
+    // if (node.internal.type === 'GroupsYaml') { createNodeField({ node, name: 'path', value: `/research/${ node.id }` }) }
+    // if (node.internal.type === 'TeamsYaml') { createNodeField({ node, name: 'path', value: `/teams/${ node.id }` }) }
+    // if (node.internal.type === 'CollaborationsYaml') { createNodeField({ node, name: 'path', value: `/collaborations/${ node.id }` }) }
+    // if (node.internal.type === 'ProjectsYaml') { createNodeField({ node, name: 'path', value: `/projects/${ node.id }` }) }
 }
 
 exports.createSchemaCustomization = ({ actions }) => {
