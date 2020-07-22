dadb5070545f4492e32de1eff43ca27cb06eb947
Matt Watson <mattwat@gmail.com>
2020-07-22 16:34:56 -0400

    use hero component on project template

--- src/templates/project-template.js
+++ src/templates/project-template.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react'
 import { graphql } from 'gatsby'
-import { Container, Article, Section, HorizontalRule } from '../components/layout'
+import { Container, Article, Section, Hero, HorizontalRule } from '../components/layout'
 import { Title, Paragraph } from '../components/typography'
 import { SocialLinks } from '../components/social-links'
 import { ArticlePreview } from '../components/news'
@@ -14,35 +14,42 @@ export default ({ data, pageContext }) => {
     }} = data
     
     return (
-        <Container>
-            <Title>{ name }</Title>
+        <Fragment>
+            <Hero>
+                <Title>{ name }</Title>
+                <Paragraph>
+                    { description }
+                </Paragraph>
+            </Hero>
 
-            <SocialLinks url={ online_presence.url } twitter={ online_presence.twitter } github={ online_presence.github } />
-            
-            {
-                news && (
-                    <Section title="News">
-                        {
-                            news.map((article, i) => {
-                                return (
-                                    <Fragment key={ article.id }>
-                                        <ArticlePreview article={ article } path={ article.fields.path } compact />
-                                        { i < news.length - 1 && <HorizontalRule /> }
-                                    </Fragment>
-                                )
-                            })
-                        }
-                    </Section>
-                )
-            }
+            <Container>
+                <SocialLinks url={ online_presence.url } twitter={ online_presence.twitter } github={ online_presence.github } />
+                
+                <Section title="Project Details">
+                    <Article title="Description">
+                        <Paragraph>{ description }</Paragraph>
+                    </Article>
+                </Section>
 
-            <Section title="Project Details">
-                <Article title="Description">
-                    <Paragraph>{ description }</Paragraph>
-                </Article>
-            </Section>
+                {
+                    news && (
+                        <Section title="News">
+                            {
+                                news.map((article, i) => {
+                                    return (
+                                        <Fragment key={ article.id }>
+                                            <ArticlePreview article={ article } path={ article.fields.path } compact />
+                                            { i < news.length - 1 && <HorizontalRule /> }
+                                        </Fragment>
+                                    )
+                                })
+                            }
+                        </Section>
+                    )
+                }
 
-        </Container>
+            </Container>
+        </Fragment>
     )
 }
 
