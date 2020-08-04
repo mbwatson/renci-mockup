3057c9308b81b0a09b2aac5428df02890ec08387
Matt Watson <mattwat@gmail.com>
2020-08-04 15:29:14 -0400

    remove translate on page content mount

--- src/components/layout/container.js
+++ src/components/layout/container.js
@@ -3,11 +3,11 @@ import styled, { css, keyframes } from 'styled-components'
 const unfade = keyframes`
     0% {
         opacity: 0.0;
-        transform: translate3d(0, -1rem, 0);
+        // transform: translate3d(0, -1rem, 0);
     }
     100% {
         opacity: 1.0;
-        transform: translate3d(0, 0, 0);
+        // transform: translate3d(0, 0, 0);
     }
 `
 
