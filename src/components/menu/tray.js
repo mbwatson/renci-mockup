59dd41f482938c49f257e0d17fc1a25459bd52ea
Matt Watson <mattwat@gmail.com>
2020-08-05 10:01:41 -0400

    flip tray

--- src/components/menu/tray.js
+++ src/components/menu/tray.js
@@ -3,9 +3,9 @@ import styled, { css, keyframes } from 'styled-components'
 import { Container } from '../layout'
 
 const reveal = keyframes`
-    0% { display: none; opacity: 0; }
-    1% { display: block; opacity: 0.0; }
-    100% { display: block; opacity: 1.0; }
+    0% { display: none; opacity: 0; transform: perspective(800px) rotate3D(1, 0, 0, 0deg)}
+    1% { display: block; opacity: 0.0; transform: perspective(800px) rotate3D(1, 0, 0, -90deg)}
+    100% { display: block; opacity: 1.0; transform: perspective(800px) rotate3D(1, 0, 0, 0deg)}
 `
 
 const Wrapper = styled.nav(({ theme }) => css`
@@ -20,6 +20,7 @@ const Wrapper = styled.nav(({ theme }) => css`
     padding: ${ theme.spacing.small } ${ theme.spacing.large };
     background-color: ${ theme.color.whitish };
     pointer-events: auto;
+    transform-origin: top center;
     animation: 250ms ${ reveal };
     &::before {
         // border: 1px solid red;
