53d955e613f7702a991438210134f1ffcb11e5f2
Matt Watson <mattwat@gmail.com>
2020-08-14 14:54:45 -0400

    add transition

--- src/components/form/select.js
+++ src/components/form/select.js
@@ -16,10 +16,10 @@ const Wrapper = styled.select(({ theme }) => `
     appearance: none;
     border: 0;
     background: url('${ chevronSvg }') 100% / 24px no-repeat transparent;
+    transition: border-color 250ms, filter 250ms;
     border-bottom: 1px solid ${ theme.color.grey };
     &:focus, &:hover {
         border-color: ${ theme.color.black };
-        filter: drop-shadow(0 0 0.1rem rgba(0, 0, 0, 0.1));
     }
 `)
 
